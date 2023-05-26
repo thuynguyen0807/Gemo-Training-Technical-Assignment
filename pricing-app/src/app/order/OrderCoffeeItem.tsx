428190a2b77bad1173a59029f20c97a5be3d9c2f
Viet-Anh, Nguyen <vietanh.dev@gmail.com>
2023-05-26 21:04:43 +0700

    Fix fetching issue - backend (#3)
    * Remove node_modules
    
    * Fix body parsing error on server side
    
    * Remove dist folder - backend
    
    * Add instructions to run to README

--- pricing-app/src/app/order/OrderCoffeeItem.tsx
+++ pricing-app/src/app/order/OrderCoffeeItem.tsx
@@ -108,14 +108,9 @@ const OrderCoffeeItem: FC<Props> = ({
   async function postData(url: string, data: any) {
     const response = await fetch(url, {
       method: "POST",
-      mode: "cors",
-      cache: "no-cache",
-      credentials: "same-origin",
       headers: {
         "Content-Type": "application/json",
       },
-      redirect: "follow", 
-      referrerPolicy: "no-referrer", 
       body: JSON.stringify(data),
     });
     return response.json();
